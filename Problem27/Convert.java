dce879ea890ba3057e9aa7c666a5faf8f3ded297
Jinsen Sheng <jinsen47@gmail.com>
2016-04-12 20:07:26 +0800

    Update Convert.java
    [BugFix] The origin solution is wrong and unreadable.

--- Problem27/Convert.java
+++ Problem27/Convert.java
@@ -3,43 +3,35 @@ package Problem27;
 public class Convert {
 	/*
 	 * 将二叉搜索树转换成一个排序的双向链表
+	 * 返回为头结点
 	 */
-	public BinaryTreeNode convert(BinaryTreeNode root)
-	{
-		BinaryTreeNode node=null;
-
-		convert(root,node);
-
-        //返回根节点
-		while(node!=null && node.leftNode!=null)
-			node=node.leftNode;
-
-		return node;
-	}
-    //重载
-    //中序遍历的格式 在中间进行操作 函数的参数可以随意改   lastNode用于保存上一个遍历的结点
-	public void convert(BinaryTreeNode root,BinaryTreeNode lastNode)
-	{
-		if(root==null)
-			return ;
-		BinaryTreeNode current=root;
-               
-		if(current.leftNode!=null)
-			convert(current.leftNode,lastNode);
-
-		current.leftNode=lastNode;
-		if(lastNode!=null)
-			lastNode.rightNode=current;
-		lastNode=current;  
-
-		if(current.rightNode!=null)
-			convert(current.rightNode,lastNode);
-	}
+	public static TreeNode convertBST(TreeNode root) {
+	        if (root == null) return null;
+	        if (root.left == null && root.right == null) return root;
+	        // 1. 左子树构造成双链表, 拿到左子树最左侧结点, 返回时作为本次获得的最左侧结点
+	        TreeNode pMostLeft = convertBST(root.left);
+	        TreeNode pCur = pMostLeft;
+	        // 2. 拿到左子树最右侧的结点, 与根节点相连
+	        while (pCur != null && pCur.right != null) {
+	            pCur = pCur.right;
+	        }
+	        if (pCur != null) {
+	            pCur.right = root;
+	            root.left = pCur;
+	        }
+		// 3. 右子树构造成双链表, 拿到最左侧结点, 与根节点相连
+	        TreeNode pRightChild = convertBST(root.right);
+	        if (pRightChild != null) {
+	            root.right = pRightChild;
+	            pRightChild.left = root;
+	        }
+	        // 4. 如果左子树为空, 返回根节点作为最左侧结点, 如果左子树不为空, 返回左子树最左侧结点
+	        return pMostLeft == null ? root : pMostLeft;
+    }
 
 }
-class BinaryTreeNode 
-{
-	int data;
-	BinaryTreeNode leftNode;
-	BinaryTreeNode rightNode;
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
 }
